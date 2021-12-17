f29b5e3b5a56bab141c28acacca86a1fc86addaf
L. Bitencourt de Sousa <89364267+malubitencourt@users.noreply.github.com>
2021-12-17 20:49:04 -0300

    finalizado e corrigido os exercícios

--- OBJETOS/01-constructor-function/script.js
+++ OBJETOS/01-constructor-function/script.js
@@ -1,91 +1,46 @@
-function initTabNav() { //função para isolar os codigos e evitar possiveis conflitos no Js
-    const tabMenu = document.querySelectorAll('.js-tabmenu li');
-    const tabContent = document.querySelectorAll('.js-tabcontent section');
-    const activeClass = 'ativo';
-
-    if(tabMenu.length && tabContent.length) { //verifica se existe itens nessa nas constantes selecionadas para depois passar os codigo dentro da verificação.
-
-        tabContent[0].classList.add(activeClass) //adiciona a classe ativo sempre no primeiro item da lista.
-
-        // função que adiciona a classe ativo no elemento selecionado e remove dos demais usando o loop.
-        function activeTab(index) { 
-            tabContent.forEach((content) => { 
-                content.classList.remove(activeClass); //loop que remove a classe das os outros elementos.
-            });
-            tabContent[index].classList.add(activeClass); //adiciona a classe.
-        };
-
-        //loop que adiciona um evento de 'click' em todos os itens que quando ocorrer  retornara a função anterior usando o index do item clicado. 
-        tabMenu.forEach((itemMenu, index) => { 
-            itemMenu.addEventListener('click', () => {
-                activeTab(index);
-            } )
-        });
-    };
+// Transforme o objeto abaixo em uma Constructor Function
+const pessoa = {
+  nome: 'Nome pessoa',
+  idade: 0,
+  andar() {
+    console.log(this.nome + ' andou');
+  }
 }
-initTabNav();
-
-
-function initAccordion(){
-    const accordionList = document.querySelectorAll('.js-accordion dt');
-    const activeClass = 'ativo';
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
-
-    function activeAccordion(event) {
-        this.classList.toggle(activeClass);
-        this.nextElementSibling.classList.toggle(activeClass);
-    };
-
-
-    accordionList.forEach((item) => {
-    item.addEventListener('click', activeAccordion);
-    });
+// Crie 3 pessoas, João - 20 anos,
+// Maria - 25 anos, Bruno - 15 anos
+function Pessoa(nome, idade) {
+  this.nome = nome,
+  this.idade = idade,
+  this.andar = function () {
+    console.log(this.nome + ' andou');
+  }
 }
-initAccordion();
+const joao = new Pessoa('João', '20 anos');
+const maria = new Pessoa('Maria', '25 anos');
+const bruno = new Pessoa('Bruno', '15 anos');
 
 
-function initScrollSuave(){
-    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+// Crie uma Constructor Function (Dom) para manipulação
+// de listas de elementos do dom. Deve conter as seguintes
+// propriedades e métodos:
+//
+// elements, retorna NodeList com os elementos selecionados
+// addClass(classe), adiciona a classe a todos os elementos
+// removeClass(classe), remove a classe a todos os elementos
 
-    function scrollToSection(event) {
-    event.preventDefault();
-        const href = event.currentTarget.getAttribute('href');
-        const section = document.querySelector(href);
-        section.scrollIntoView({
-            behavior:'smooth',
-            block: 'start',
-  }); 
-
-    //forma alternativa
-        // const topo = section.offsetTop;
-        // window.scrollTo({
-        //     top: topo,
-        //     behavior:'smooth',
-        // });
-    }
-
-    linksInternos.forEach((link) =>{
-        link.addEventListener('click', scrollToSection);
+function Dom(selector) {
+  const elementList = document.querySelectorAll(selector);
+  this.elements = elementList;
+  
+  this.addClass = function(classe) {
+    elementList.forEach((element) => { 
+      element.classList.add(classe);
     });
+  },
+  this.removeClass = function(classe) {
+    elementList.forEach((classe) => {
+      element.classList.remove(classe);
+  });
+  }
 };
-initScrollSuave();
-
-function initAnimaScroll(){ 
-    const sections = document.querySelectorAll('.js-scroll');
-    if(sections.length) {
-    const windowMetade = window.innerHeight * 0.6;
-    function animaScroll(){
-        sections.forEach((section) => {
-            const sectionTop = section.getBoundingClientRect().top;
-            const isSectionVisible = (sectionTop - windowMetade) < 0;
-            if(isSectionVisible) {
-              section.classList.add('ativo');
-            };
-        })    
-    };
-    animaScroll();
-    window.addEventListener('scroll', animaScroll);
-};
-};
-initAnimaScroll();
+const listaItens = new Dom('li');
