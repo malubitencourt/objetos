bb51cba84a1ec1db20f3c3f2e4427281cd58fbb6
L. Bitencourt de Sousa <89364267+malubitencourt@users.noreply.github.com>
2021-12-26 21:36:26 -0300

    atividades da aula de strings

---
+++ OBJETOS/04-string/script.js
@@ -0,0 +1,21 @@
+// const comida = 'pizza';
+// const liquido = new String('Água');
+
+// console.log(comida.length);
+
+// const frase = 'A melhor comida';
+
+// console.log(frase[frase.length - 1]);
+// console.log(frase.charAt[1])
+
+const frase = 'A melhor linguaguem é ';
+const linguaguem = 'JavaScript';
+
+const fraseFinal = frase.concat(linguaguem, '!!', ' gostei da camisa' );
+console.log(fraseFinal);
+
+const fruta = 'Banana';
+const listaFrutas  = 'Melancia, Banana, laranja';
+
+console.log(listaFrutas.includes(fruta, 14));
+console.log(fruta.includes(listaFrutas));
