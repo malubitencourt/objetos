2b7e4f75404b13356f49f0b67ffb2bdf4c040dbc
L. Bitencourt de Sousa <89364267+malubitencourt@users.noreply.github.com>
2022-01-04 21:04:29 -0300

    Resolvido 1º e 2º quesito do exercício

--- OBJETOS/05-number-e-math/script.js
+++ OBJETOS/05-number-e-math/script.js
@@ -1,10 +1,13 @@
 // Retorne um número aleatório
 // entre 1050 e 2000
-
+const numRand = Math.floor(Math.random() * ( 2000 - 1050 + 1) + 1050);
+console.log(numRand);
 
 // Retorne o maior número da lista abaixo
 const numeros = '4, 5, 20, 8, 9';
-
+const arrayNumeros = numeros.split(', ');
+const numeroMax = Math.max(...arrayNumeros);
+console.log(numeroMax)
 
 // Crie uma função para limpar os preços
 // e retornar os números com centavos arredondados
